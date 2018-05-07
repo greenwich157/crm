af3bd6b75ff1b62b8f3c86118c7a208a8bdb2ba7
greenwich157 <greenwich157@gmail.com>
2018-05-07 15:23:14 +0800

    Update HelloController.java

--- src/main/java/com/oc/restful/hello/HelloController.java
+++ src/main/java/com/oc/restful/hello/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	
 	@RequestMapping("/")
 	public String greetings() throws InterruptedException {
 		// Thread.sleep(3000);
